e14fe9bd3259aee750d33c726316d611ba157562
Kaveesha Sachini <kaveesachee@gmail.com>
2026-04-30 15:35:48 +0530

    chore: set missed check-in grace period to 5 minutes

---
+++ lib/core/constants/night_mode_constants.dart
@@ -0,0 +1,11 @@
+class NightModeConstants {
+// Default Scheduling Hours 
+]
+static const int defaultStartHour = 22;
+// 10:00 PM 
+static const int defaultEndHour = 6;    
+// 06:00 AM // Escalation Logic 
+static const int missedCheckInGracePeriodMinutes = 5; 
+static const String escalationAlertTitle = 'NIGHT MODE: Missed Check-in'; 
+
+}
